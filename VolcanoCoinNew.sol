2787081bd7a11869ea308ce2506b313fee2c5c5e
JohnGkliatis <57404941+JohnGkliatis@users.noreply.github.com>
2022-10-25 22:03:46 +0100

    Create VolcanoCoinNew.sol
    Ownership is being inherited by openzeppelin Ownable.sol

---
+++ VolcanoCoinNew.sol
@@ -0,0 +1,58 @@
+//Homework 5
+//2. Update your Volcano coin contract to inherit from the Open Zeppelin Ownable
+//contract, and use this to replace the owner functionality in your contract.
+
+
+// SPDX-License-Identifier: UNLICENCED
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract VolcanoCoin is Ownable {
+    
+
+    uint256 totalSupply = 10000;
+
+    constructor() Ownable () {
+        balances[owner()] = totalSupply;
+        totalSupply = 0;
+    }
+
+
+
+    function retrieveTotalSupply() public view returns (uint256) {
+        return totalSupply;
+    }
+
+    event currentSupply(uint256);
+
+    function addAmount() public onlyOwner {
+        totalSupply += 1000;
+        emit currentSupply(totalSupply);
+    }
+
+    mapping(address => uint256) public balances;
+
+    event transfer(address , uint256);
+
+    function transferFunds(address _recipient, uint256 _amount) public {
+        require(_amount > 0,"Amount must be > 0");
+        require(balances[msg.sender] >= _amount,"Insufficient funds");
+        balances[_recipient] += _amount;
+        balances[msg.sender] -= _amount;
+        paymentsHistory[msg.sender].push(Payment({recipient:_recipient, amount:_amount }));
+        emit transfer(_recipient, _amount);
+    }
+
+    struct Payment {
+        address recipient;
+        uint amount;
+    }
+
+    mapping (address => Payment[]) public paymentsHistory;
+
+    function getpaymentsHistory(address _user) public view returns(Payment[] memory) {
+        return paymentsHistory[_user];
+    }
+}
